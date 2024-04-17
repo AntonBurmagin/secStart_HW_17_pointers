3dde3e30c83240b526fb9afb188081fb4fdb58d3
Anton Burmagin <burmagin.anton.s@gmail.com>
2024-04-17 22:10:04 +0300

    initial commit

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+void swap (int *a, int *b) {
+	int c = *a;
+	// *a = b;
+	// *b = c;
+}
+
+void arraySwap (int *arr) {
+	for (int i = 0; i < 5; i++) {
+		swap(arr+i, arr + 10 - i*2);
+	}
+}
+
+int main() {
+	//17.4.1
+	int a = 10;
+	int b = 20;
+	// swap(a, b);
+	std::cout << a << " " << b;
+	
+	//17.4.2
+	int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	arraySwap(arr);
+	
+	return 0;
+}
